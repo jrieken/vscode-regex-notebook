c603afddcf317b42f05199ba86cfac0387431728
Johannes Rieken <johannes.rieken@gmail.com>
2021-04-12 15:00:39 +0200

    update to latest API etc

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,105 +1,86 @@
+import { TextDecoder } from 'node:util';
 import * as vscode from 'vscode';
+
+
+const serializer = new class implements vscode.NotebookSerializer {
+
+	dataToNotebook(data: Uint8Array): vscode.NotebookData {
+		const str = new TextDecoder().decode(data);
+		const cells = JSON.parse(str);
+		return new vscode.NotebookData(cells);
+	}
+
+	notebookToData(data: vscode.NotebookData): Uint8Array {
+		const cells = data.cells;
+		const str = JSON.stringify(cells, undefined, 2);
+		return Buffer.from(str);
+	}
+};
 
 const contentProvider = new class implements vscode.NotebookContentProvider {
 
-	private readonly _onDidChangeNotebook = new vscode.EventEmitter<vscode.NotebookDocumentContentChangeEvent>();
-
-	readonly onDidChangeNotebook = this._onDidChangeNotebook.event;
-
-	async openNotebook(uri: vscode.Uri, openContext: vscode.NotebookDocumentOpenContext): Promise<vscode.NotebookData> {
-
-		const actual = openContext.backupId ? vscode.Uri.parse(openContext.backupId) : uri;
-		const data = Buffer.from(await vscode.workspace.fs.readFile(actual)).toString();
-
-		let raw: { cellKind: number; source: string }[];
-		try {
-			raw = JSON.parse(data);
-		} catch {
-			raw = [];
+	async openNotebook(uri: vscode.Uri, openContext: vscode.NotebookDocumentOpenContext, token: vscode.CancellationToken) {
+		let data = openContext.untitledDocumentData;
+		if (!data) {
+			let fileUri: vscode.Uri;
+			if (openContext.backupId) {
+				fileUri = vscode.Uri.parse(openContext.backupId);
+			} else {
+				fileUri = uri;
+			}
+			data = await vscode.workspace.fs.readFile(fileUri);
 		}
-
-		const cells: vscode.NotebookCellData[] = raw.map(cell => {
-			return {
-				cellKind: cell.cellKind,
-				source: cell.source,
-				language: cell.cellKind === vscode.CellKind.Code ? 'plaintext' : 'markdown',
-				outputs: [{ outputKind: vscode.CellOutputKind.Rich, data: { 'x-application/regex': cell.source } }],
-				metadata: undefined //TODO@jrieken mark with ?
-			};
-		});
-
-		return {
-			cells,
-			languages: ['plaintext'],
-			metadata: {}
-		};
+		const str = new TextDecoder().decode(data);
+		const cells = JSON.parse(str);
+		return new vscode.NotebookData(cells);
 	}
-
-	async resolveNotebook(_document: vscode.NotebookDocument, _webview: vscode.NotebookCommunication): Promise<void> {
-		// nothing
+	saveNotebook(document: vscode.NotebookDocument, token: vscode.CancellationToken) {
+		return this.saveNotebookAs(document.uri, document, token);
 	}
-
-	async backupNotebook(document: vscode.NotebookDocument, context: vscode.NotebookDocumentBackupContext, token: vscode.CancellationToken): Promise<vscode.NotebookDocumentBackup> {
+	async saveNotebookAs(targetResource: vscode.Uri, document: vscode.NotebookDocument, token: vscode.CancellationToken) {
+		const cells = document.getCells();
+		const str = JSON.stringify(cells, undefined, 2);
+		const data = Buffer.from(str);
+		await vscode.workspace.fs.writeFile(targetResource, data);
+	}
+	async backupNotebook(document: vscode.NotebookDocument, context: vscode.NotebookDocumentBackupContext, token: vscode.CancellationToken) {
 		await this.saveNotebookAs(context.destination, document, token);
 		return {
 			id: context.destination.toString(),
 			delete() { vscode.workspace.fs.delete(context.destination); }
 		};
 	}
-
-	saveNotebook(document: vscode.NotebookDocument, token: vscode.CancellationToken): Promise<void> {
-		return this.saveNotebookAs(document.uri, document, token);
-	}
-
-	async saveNotebookAs(targetResource: vscode.Uri, document: vscode.NotebookDocument, _token: vscode.CancellationToken): Promise<void> {
-		const lines: { cellKind: number; source: string }[] = [];
-		for (const cell of document.cells) {
-			lines.push({ cellKind: cell.cellKind, source: cell.document.getText() });
-		}
-		const data = Buffer.from(JSON.stringify(lines, undefined, 2));
-		await vscode.workspace.fs.writeFile(targetResource, data);
-	}
 };
 
 const kernel = new class implements vscode.NotebookKernel {
 
+	readonly id = 'regex';
 	readonly label: string = 'Regex';
-
+	readonly supportedLanguages = ['plaintext'];
 	private _execOrderPool = 0;
 
-	async executeCell(document: vscode.NotebookDocument, cell: vscode.NotebookCell): Promise<void> {
-		const edit = new vscode.WorkspaceEdit();
-		const output = new vscode.NotebookCellOutputItem('x-application/regex', cell.document.getText());
-		edit.replaceNotebookCellOutput(document.uri, cell.index, [new vscode.NotebookCellOutput([output])]);
-		edit.replaceNotebookCellMetadata(document.uri, cell.index, { executionOrder: this._execOrderPool++ });
-		await vscode.workspace.applyEdit(edit);
-	}
+	async executeCellsRequest(document: vscode.NotebookDocument, ranges: vscode.NotebookCellRange[]) {
 
-	executeAllCells(document: vscode.NotebookDocument): void {
-		for (const cell of document.cells) {
-			if (cell.cellKind === vscode.CellKind.Code) {
-				this.executeCell(document, cell);
+		for (const range of ranges) {
+			const cells = document.getCells(range);
+			for (const cell of cells) {
+				const exec = vscode.notebook.createNotebookCellExecutionTask(document.uri, cell.index, this.id)!;
+				exec.executionOrder = this._execOrderPool++;
+				exec.start();
+				const output = new vscode.NotebookCellOutputItem('x-application/regex', cell.document.getText());
+				exec.replaceOutput([new vscode.NotebookCellOutput([output])]);
+				exec.end();
 			}
 		}
-	}
-
-	cancelCellExecution(document: vscode.NotebookDocument, cell: vscode.NotebookCell): void {
-		throw new Error('Method not implemented.');
-	}
-
-	cancelAllCellsExecution(document: vscode.NotebookDocument): void {
-		throw new Error('Method not implemented.');
 	}
 };
 
 export function activate(_context: vscode.ExtensionContext) {
 
-	vscode.notebook.registerNotebookContentProvider('regexp-np', contentProvider);
+	vscode.notebook.registerNotebookSerializer('regexp-np', serializer);
+	// vscode.notebook.registerNotebookContentProvider('regexp-np', contentProvider);
+
 	vscode.notebook.registerNotebookKernelProvider({ viewType: 'regexp-np' }, new class implements vscode.NotebookKernelProvider {
-
-		//TODO@jrieken overkill, replace with notebook runner concept?
-		// onDidChangeKernels?: vscode.Event<vscode.NotebookDocument | undefined> | undefined;
-
 		provideKernels(document: vscode.NotebookDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.NotebookKernel[]> {
 			return [kernel];
 		}
